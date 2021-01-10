035491f65ef58333f9805a6ee97db35d64891039
zpgy77@gmail.com <zpgy77@gmail.com>
2021-01-10 16:57:41 +0900

    add analysis

--- TrappingRainWater/main.cpp
+++ TrappingRainWater/main.cpp
@@ -1,3 +1,17 @@
+/**
+  * Runtime: 4 ms, faster than 98.86% of C++ online submissions for Trapping Rain Water.
+  * Memory Usage: 14.5 MB, less than 51.93% of C++ online submissions for Trapping Rain Water.
+  *
+  * Time Complexity: O(n), n: number of elementes in input vector
+  * - read input vector twice. From left to right and right to left
+  *
+  * Space Complexity: O(n)
+  * - need additional array of size n to keep track of heightAfterRain
+  *
+  * algorithm
+  * - heightAfterRain[i] = min(max(Height, 0...i), max(Height, (i + 1)..<n))
+*/
+
 #include "vector"
 using namespace std;
 
@@ -9,13 +23,13 @@ public:
         int heightAfterRain[n];
         int curMax = 0;
         int res = 0;
-        
+
         for (int i = 0; i < n; i++)
         {
             if (height[i] > curMax) curMax = height[i];
             heightAfterRain[i] = curMax;
         }
-        
+
         curMax = 0;
         for (int i = n - 1; i >= 0; i--)
         {
