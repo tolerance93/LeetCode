c179006c444aa2d7532631c9cce7e5cb9b3d1e87
zpgy77@gmail.com <zpgy77@gmail.com>
2021-02-10 10:23:25 +0900

    Problem #714

--- Best Time to Buy and Sell Stock with Transaction Fee/Best Time to Buy and Sell Stock with Transaction Fee/StateMachine.cpp
+++ Best Time to Buy and Sell Stock with Transaction Fee/Best Time to Buy and Sell Stock with Transaction Fee/StateMachine.cpp
@@ -1,13 +1,17 @@
+/**
+ * Runtime: 88 ms, faster than 93.42% of C++ online submissions for Best Time to Buy and Sell Stock with Transaction Fee.
+ * Memory Usage: 55.3 MB, less than 66.79% of C++ online submissions for Best Time to Buy and Sell Stock with
+ * Transaction Fee.
+ */
+
 #include <iostream>
 #include <vector>
 
 using namespace std;
 
-class Solution
-{
+class Solution {
 public:
-    int maxProfit(vector<int>& prices, int fee)
-    {
+    int maxProfit(vector<int>& prices) {
         int n = prices.size();
         if (n == 0) return 0;
         
@@ -20,7 +24,7 @@ public:
         
         for (int i = 1; i < n; i++)
         {
-            state_can_buy[i] = max(state_can_buy[i - 1], state_can_sell[i - 1] + prices[i] - fee);
+            state_can_buy[i] = max(state_can_buy[i - 1], state_can_sell[i - 1] + prices[i]);
             state_can_sell[i] = max(state_can_sell[i - 1], state_can_buy[i - 1] - prices[i]);
         }
         res = max(res, state_can_buy[n - 1]);
@@ -31,8 +35,8 @@ public:
 
 int main()
 {
-    vector<int> input = {1, 3, 2, 8, 4, 9};
+    vector<int> input = {7,1,5,3,6,4};
     Solution sol;
-    cout <<sol.maxProfit(input, 2) << endl;
+    cout <<sol.maxProfit(input) << endl;
     return 0;
 }
