d2b5d85d3c61398d1476847dbb2f4cf413806611
zpgy77@gmail.com <zpgy77@gmail.com>
2021-02-11 18:52:11 +0900

    Linked List Solution

--- Daily Temperatures/Daily Temperatures/main.cpp
+++ Daily Temperatures/Daily Temperatures/main.cpp
@@ -6,7 +6,8 @@ using namespace std;
 /**
  * Brute force: Time out!!
  */
-class Solution {
+
+class Solution1 {
 public:
     vector<int> dailyTemperatures(vector<int>& T) {
         int n = T.size();
@@ -23,6 +24,100 @@ public:
                 }
             }
             if (res.size() == i) res.push_back(0);
+        }
+        return res;
+    }
+};
+/**
+ * Linked list solution
+ */
+
+class Solution
+{
+private:
+    const static int MAX_TEMPARATUE = 100;
+    const static int NULL_INDEX = 40000;
+    struct Node
+    {
+        int pos;
+        Node* next;
+    };
+    struct NodeList
+    {
+        Node* head;
+        Node* tail;
+        
+        NodeList() : head(0), tail(0) {}
+        NodeList(Node* head, Node* tail) : head(head), tail(tail) {}
+    };
+public:
+    vector<int> dailyTemperatures(vector<int>& T) {
+        int n = T.size();
+        NodeList tempatatures[MAX_TEMPARATUE + 1];
+        vector<int> res;
+        
+        for (int i = 0; i < MAX_TEMPARATUE + 1; i++)
+        {
+            tempatatures[i] = {0, 0};
+        }
+        
+        for (int i = 0; i < n; i++)
+        {
+            Node* node = new Node;
+            node->next = 0;
+            node->pos = i;
+            
+            if (tempatatures[T[i]].tail)
+            {
+                tempatatures[T[i]].tail->next = node;
+                tempatatures[T[i]].tail = node;
+            }
+            else
+            {
+                tempatatures[T[i]].head = node;
+                tempatatures[T[i]].tail = node;
+            }
+        }
+        
+        for (int i = 0; i < n; i++)
+        {
+            int pos = NULL_INDEX;
+            for (int j = T[i] + 1; j <= 100; j++)
+            {
+                if (tempatatures[j].head)
+                {
+                    Node* prev = 0;
+                    Node* cur = tempatatures[j].head;
+                    while (cur && cur->pos <= i)
+                    {
+                        prev = cur;
+                        cur = cur->next;
+                        delete prev;
+                        prev = 0;
+                    }
+                    tempatatures[j].head = cur;
+                    if (!cur) tempatatures[j].tail = 0;
+                    
+                    if (cur && cur->pos < pos) pos = cur->pos;
+                }
+            }
+            if (pos != NULL_INDEX) res.push_back(pos - i);
+            else res.push_back(0);
+        }
+        //Prevent memory leak
+        for (int i = 0; i < MAX_TEMPARATUE + 1; i++)
+        {
+            if (tempatatures[i].head)
+            {
+                Node* prev = 0;
+                Node* cur = tempatatures[i].head;
+                while (cur)
+                {
+                    prev = cur;
+                    cur = cur->next;
+                    delete prev;
+                }
+            }
         }
         return res;
     }
