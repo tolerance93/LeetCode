7c04b2df5c1e35878a92632af7c3804b4e9bb5ef
zpgy77@gmail.com <zpgy77@gmail.com>
2021-02-11 14:09:44 +0900

    Brute force solution. Timeout

---
+++ Daily Temperatures/Daily Temperatures/main.cpp
@@ -0,0 +1,41 @@
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+/**
+ * Brute force: Time out!!
+ */
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& T) {
+        int n = T.size();
+        vector<int> res;
+        
+        for (int i = 0; i < n ; i++)
+        {
+            for (int j = i + 1; j < n; j++)
+            {
+                if (T[j] > T[i])
+                {
+                    res.push_back(j - i);
+                    break;
+                }
+            }
+            if (res.size() == i) res.push_back(0);
+        }
+        return res;
+    }
+};
+
+int main()
+{
+    vector<int> input = {73,74,75,71,69,72,76,73};
+    Solution sol;
+    vector<int> res = sol.dailyTemperatures(input);
+    for (int i = 0; i < res.size(); i++)
+    {
+        cout << res[i] << endl;
+    }
+    return 0;
+}
