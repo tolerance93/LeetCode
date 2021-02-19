ac7870121c158923488a49d45c66cc1db3aad9ec
zpgy77@gmail.com <zpgy77@gmail.com>
2021-02-19 22:53:48 +0900

    Add analysis

--- Best Time to Buy and Sell Stock III/Best Time to Buy and Sell Stock III/SearchBackAndFront.cpp
+++ Best Time to Buy and Sell Stock III/Best Time to Buy and Sell Stock III/SearchBackAndFront.cpp
@@ -1,6 +1,21 @@
 /**
  * Runtime: 128 ms, faster than 90.53% of C++ online submissions for Best Time to Buy and Sell Stock III.
  * Memory Usage: 78.4 MB, less than 49.45% of C++ online submissions for Best Time to Buy and Sell Stock III.
+ *
+ * [Time Complexity]
+ * O(n): Iterates input array twice.
+ *
+ * [Space Complexity]
+ * O(n): need array of size n to store max value
+ *
+ * [Algorithm]
+ * Let maxProfit(range) denotes max profit gained from one transaction at given range.
+ *
+ * 1. Iterate input array from back to front.
+ *   maxArray[i] stores maxProfit(i <= x < end).
+ * 2. Iterate input array from front to back.
+ *   maxArray[i] stores maxProfit(0 <= x < i) + maxProfit(i <= x < end).
+ *   Update result value.
  */
 
 #include <iostream>
