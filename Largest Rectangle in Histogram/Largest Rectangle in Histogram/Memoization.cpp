833e456762246b676de1e4f8a63040f84a14d8ec
zpgy77@gmail.com <zpgy77@gmail.com>
2021-03-07 15:10:59 +0900

    Add analysis

--- Largest Rectangle in Histogram/Largest Rectangle in Histogram/Memoization.cpp
+++ Largest Rectangle in Histogram/Largest Rectangle in Histogram/Memoization.cpp
@@ -3,7 +3,6 @@
  * Memory Usage: 62.4 MB, less than 34.21% of C++ online submissions for Largest Rectangle in Histogram.
  */
 
-
 #include <vector>
 #include <iostream>
 
@@ -14,19 +13,9 @@ public:
     int largestRectangleArea(vector<int>& heights) {
         int n = heights.size();
         if (n == 0) return 0;
-        
-        int leftEnd[n];
-        int rightEnd[n];
-        int res = 0;
-        
-        for (int i = 0; i < n; i++)
-        {
-            leftEnd[i] = 0;
-            rightEnd[i] = 0;
-        }
-        
+    
+        vector<int> leftEnd(n, 0);
         leftEnd[0] = -1;
-        rightEnd[n - 1] = n;
         
         for (int i = 1; i < n; i++)
         {
@@ -38,6 +27,9 @@ public:
             leftEnd[i] = j;
         }
         
+        vector<int> rightEnd(n, 0);
+        rightEnd[n - 1] = n;
+        
         for (int i = n - 2; i >= 0; i--)
         {
             int j = i + 1;
@@ -47,6 +39,8 @@ public:
             }
             rightEnd[i] = j;
         }
+        
+        int res = 0;
         
         for (int i = 0; i < n; i++)
         {
