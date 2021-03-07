0a2712a01a11f55d0d5d80a187e8397b7d8f9ae4
zpgy77@gmail.com <zpgy77@gmail.com>
2021-03-07 10:51:00 +0900

    Memoization Solution

---
+++ Largest Rectangle in Histogram/Largest Rectangle in Histogram/Memoization.cpp
@@ -0,0 +1,65 @@
+/**
+ * Runtime: 100 ms, faster than 49.62% of C++ online submissions for Largest Rectangle in Histogram.
+ * Memory Usage: 62.4 MB, less than 34.21% of C++ online submissions for Largest Rectangle in Histogram.
+ */
+
+
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& heights) {
+        int n = heights.size();
+        if (n == 0) return 0;
+        
+        int leftEnd[n];
+        int rightEnd[n];
+        int res = 0;
+        
+        for (int i = 0; i < n; i++)
+        {
+            leftEnd[i] = 0;
+            rightEnd[i] = 0;
+        }
+        
+        leftEnd[0] = -1;
+        rightEnd[n - 1] = n;
+        
+        for (int i = 1; i < n; i++)
+        {
+            int j =  i - 1;
+            while (j >= 0 && heights[j] >= heights[i])
+            {
+                j = leftEnd[j];
+            }
+            leftEnd[i] = j;
+        }
+        
+        for (int i = n - 2; i >= 0; i--)
+        {
+            int j = i + 1;
+            while (j < n && heights[i] <= heights[j])
+            {
+                j = rightEnd[j];
+            }
+            rightEnd[i] = j;
+        }
+        
+        for (int i = 0; i < n; i++)
+        {
+            res = max(res, heights[i] * (rightEnd[i] - leftEnd[i] - 1));
+        }
+        
+        return res;
+    }
+};
+
+int main()
+{
+    Solution sol;
+    vector<int> input = { 2,1,5,6,2,3 };
+    cout << sol.largestRectangleArea(input) << endl;
+}
