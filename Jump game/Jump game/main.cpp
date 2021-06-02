f0985f829866f311fe965f8140a6f6139232919a
Tolerance <zpgy77@gmail.com>
2021-06-02 23:04:55 +0900

    Add Forward Solution

--- Jump game/Jump game/main.cpp
+++ Jump game/Jump game/main.cpp
@@ -31,3 +31,28 @@ public:
         return res;
     }
 };
+
+/**
+ * Runtime: 56 ms, faster than 32.87% of C++ online submissions for Jump Game.
+ * Memory Usage: 48.4 MB, less than 9.13% of C++ online submissions for Jump Game.
+ * 비슷함..
+ */
+class Solution2
+{
+public:
+    bool canJump(vector<int>& nums)
+    {
+        int jump = 0;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            jump--;
+            jump = nums[i] > jump ? nums[i] : jump;
+            if (nums[i] == 0 && jump <= 0)
+            {
+                if (i == nums.size() - 1) return true;
+                else return false;
+            }
+        }
+        return true;
+    }
+};
