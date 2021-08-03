973c0c5b05589f58e9b52093c6631a8bf79545d0
Tolerance <zpgy77@gmail.com>
2021-08-03 22:28:10 +0900

    72. Edit Distance

---
+++ Edit Distance/Edit Distance/main.cpp
@@ -0,0 +1,50 @@
+/**
+ * Runtime: 8 ms, faster than 82.24% of C++ online submissions for Edit Distance.
+ * Memory Usage: 7.3 MB, less than 62.75% of C++ online submissions for Edit Distance.
+ */
+
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        int m = word1.size();
+        int n = word2.size();
+        int dp[m + 1][n + 1];
+        
+        for (int i = 0; i <= m; i++)
+        {
+            for (int j = 0; j <= n; j++)
+            {
+                if (i == 0)
+                {
+                    dp[i][j] = j;
+                }
+                else if (j == 0)
+                {
+                    dp[i][j] = i;
+                }
+                else if (word1[i - 1] == word2[j - 1])
+                {
+                    dp[i][j] = dp[i - 1][j - 1];
+                }
+                else
+                {
+                    int insert = dp[i][j - 1];
+                    int del = dp[i - 1][j];
+                    int replace = dp[i - 1][j - 1];
+                    dp[i][j] = 1 + min(insert, min(del, replace));
+                    
+                }
+            }
+        }
+        return dp[m][n];
+    }
+};
+
+int main()
+{
+    return 0;
+}
