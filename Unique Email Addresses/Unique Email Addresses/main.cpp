def77eacbe97389d918d4787f3c8b26c914ae2d5
Tolerance <zpgy77@gmail.com>
2022-02-03 14:32:14 +0900

    Refactor for Readabliity

--- Unique Email Addresses/Unique Email Addresses/main.cpp
+++ Unique Email Addresses/Unique Email Addresses/main.cpp
@@ -12,39 +12,59 @@ using namespace std;
 
 class Solution {
 public:
+    
+    int findAtPosition(string& email)
+    {
+        for (int i = 0; i < email.length(); i++)
+        {
+            if (email[i] == '@')
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+    
+    string extractDomainWithAtPostion(string& email, int i)
+    {
+        return email.substr(i + 1, email.length());
+    }
+    
+    string extractLocalWithAtPostion(string& email, int i)
+    {
+        return email.substr(0, i);
+    }
+    
+    string extractUniqueLocal(string& local)
+    {
+        string uniqueLocal;
+        for (char c : local) {
+            if (c == '+') {
+                break;
+            }
+            else if (c == '.') {
+                continue;
+            }
+            else
+            {
+                uniqueLocal.push_back(c);
+            }
+        }
+        return uniqueLocal;
+    }
+    
     int numUniqueEmails(vector<string>& emails) {
         unordered_set<string> set;
         string domain;
         string local;
-        
+        string uniqueEmail;
         for (string& email : emails)
         {
-            for (int i = 0; i < email.length(); i++)
-            {
-                if (email[i] == '@')
-                {
-                    domain = email.substr(i + 1, email.length());
-                    local = email.substr(0, i);
-                }
-            }
-            string uniqueLocal;
-            for (char c : local) {
-                if (c == '+') {
-                    break;
-                }
-                else if (c == '.') {
-                    continue;
-                }
-                else
-                {
-                    uniqueLocal.push_back(c);
-                }
-            }
-            string fullname = string(uniqueLocal) + '@' + domain;
-            if (set.find(fullname) == set.end())
-            {
-                set.insert(fullname);
-            }
+            int atPosition = findAtPosition(email);
+            domain = extractDomainWithAtPostion(email, atPosition);
+            local = extractLocalWithAtPostion(email, atPosition);
+            uniqueEmail = extractUniqueLocal(local) + '@' + domain;
+            set.insert(uniqueEmail);
         }
         return (int) set.size();
     }
