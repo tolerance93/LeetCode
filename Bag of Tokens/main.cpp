97c1c8df5442ca6f0873820ac58e5c103e993f11
Tolerance <zpgy77@gmail.com>
2023-03-05 14:42:21 +0900

    948. Bag of Tokens

---
+++ Bag of Tokens/main.cpp
@@ -0,0 +1,14 @@
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    int bagOfTokensScore(vector<int>& tokens, int power) {
+        sort(tokens.begin(), tokens.end());
+
+        return 0;
+    }
+};
