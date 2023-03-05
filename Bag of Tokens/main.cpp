01ccb3da4b175406680c1fa1be6810897c3173d2
Tolerance <zpgy77@gmail.com>
2023-03-05 14:48:46 +0900

    reduce condition indent

--- Bag of Tokens/main.cpp
+++ Bag of Tokens/main.cpp
@@ -7,8 +7,31 @@ using namespace std;
 class Solution {
 public:
     int bagOfTokensScore(vector<int>& tokens, int power) {
+        int score = 0;
         sort(tokens.begin(), tokens.end());
-
-        return 0;
+        int head = 0;
+        int tail = tokens.size() - 1;
+        int res = 0;
+        while (head <= tail)
+        {
+            if (power >= tokens[head])
+            {
+                score += 1;
+                res = max(res, score);
+                power -= tokens[head];
+                head++;
+            }
+            else if (score > 0)
+            {
+                score -= 1;
+                power += tokens[tail];
+                tail--;
+            }
+            else
+            {
+                break;
+            }
+        }
+        return res;
     }
 };
